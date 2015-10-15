95b14c7e8571809a003fd0a222367a38fec64ddd
talumbau <tj.alumbaugh@continuum.io>
2015-10-14 21:03:30 -0500

    fixed migrations

---
+++ webapp/apps/taxbrain/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taxbrain', '0005_auto_20151014_2058'),
+        ('taxbrain', '0006_auto_20151014_1211'),
+    ]
+
+    operations = [
+    ]
