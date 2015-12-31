b82f0f4a660be75fbdf6d624d08f22599764fbd9
talumbau <tj.alumbaugh@continuum.io>
2015-12-31 16:45:49 -0600

    Adds dumped JSON data of flatblocks on heroku
    - also adds a 'sendfile.py' script to send the data to S3 once it is
      dumped ot a file on Heroku

---
+++ sendfile.py
@@ -0,0 +1,20 @@
+#python manage.py dumpdata flatblocks --output flatblocks_heroku.json
+
+import boto
+from boto.s3.connection import S3Connection
+import os
+
+AWS_KEY_ID = os.environ['AWS_KEY_ID']
+AWS_SECRET_ID = os.environ['AWS_SECRET_ID']
+
+aws_connection = S3Connection(AWS_KEY_ID, AWS_SECRET_ID)
+bucket = aws_connection.get_bucket('pufbucket')
+
+from boto.s3.key import Key
+k = Key(bucket)
+k.key = 'flatblocks_heroku'
+
+flatblocks_info = open("flatblocks_heroku.json").read()
+k.set_contents_from_string(flatblocks_info)
+
+
