45023e1cbb70355241ad8871824b69a2b8bfae92
talumbau <tj.alumbaugh@continuum.io>
2016-02-28 22:05:41 -0600

    remove debug statement

--- webapp/apps/taxbrain/tests.py
+++ webapp/apps/taxbrain/tests.py
@@ -186,7 +186,6 @@ class TaxInputTests(TestCase):
     def test_package_up_vars_multi_year(self):
         user_values = {'SS_Earnings_c': [118500, 999999]}
         ans = package_up_vars(user_values, first_budget_year=2016)
-        import pdb;pdb.set_trace()
         assert ans['_SS_Earnings_c'] == [118500.0, 999999.0]
 
     def test_expand1d(self):
