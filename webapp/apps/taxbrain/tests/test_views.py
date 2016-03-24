765d4fee981c9ff39fbcdf9a8fb54e2b709f2645
talumbau <tj.alumbaugh@continuum.io>
2016-03-23 21:16:33 -0500

    Only read JSON response after checking status code
    - Fixes a bug that would crash the app if attempting to submit to a node
      that is misbehaving. We only look at the contents of the repsonse if
      we get a 200 from the worker node

--- webapp/apps/taxbrain/tests/test_views.py
+++ webapp/apps/taxbrain/tests/test_views.py
@@ -6,7 +6,8 @@ from ..models import TaxSaveInputs
 from ..models import convert_to_floats
 from ..helpers import (expand_1D, expand_2D, expand_list, package_up_vars,
                      format_csv, arrange_totals_by_row, default_taxcalc_data)
-from ..compute import DropqCompute, MockCompute, MockFailedCompute
+from ..compute import (DropqCompute, MockCompute, MockFailedCompute,
+                       NodeDownCompute)
 import taxcalc
 from taxcalc import Policy
 from .utils import *
@@ -72,6 +73,32 @@ class TaxBrainViewsTests(TestCase):
         # Check that we get a 400
         self.assertEqual(response.status_code, 400)
 
+    def test_taxbrain_nodes_down(self):
+        #Monkey patch to mock out running of compute jobs
+        import sys
+        from webapp.apps.taxbrain import views as webapp_views
+        webapp_views.dropq_compute = NodeDownCompute()
+
+        data = {u'ID_BenefitSurtax_Switch_1': [u'True'],
+                u'ID_BenefitSurtax_Switch_0': [u'True'],
+                u'ID_BenefitSurtax_Switch_3': [u'True'],
+                u'ID_BenefitSurtax_Switch_2': [u'True'],
+                u'ID_BenefitSurtax_Switch_5': [u'True'],
+                u'ID_BenefitSurtax_Switch_4': [u'True'],
+                u'ID_BenefitSurtax_Switch_6': [u'True'],
+                u'has_errors': [u'False'], u'II_em': [u'4333'],
+                u'start_year': unicode(START_YEAR), 'csrfmiddlewaretoken':'abc123'}
+
+        response = self.client.post('/taxbrain/', data)
+        # Check that redirect happens
+        self.assertEqual(response.status_code, 302)
+        link_idx = response.url[:-1].rfind('/')
+        self.failUnless(response.url[:link_idx+1].endswith("taxbrain/"))
+        # One more redirect
+        response = self.client.get(response.url)
+        # Check that we successfully load the page
+        response = self.client.get(response.url)
+        self.assertEqual(response.status_code, 200)
 
     def test_taxbrain_failed_job(self):
         #Monkey patch to mock out running of compute jobs
