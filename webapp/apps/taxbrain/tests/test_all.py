f0bf1f1333994d973569047e8d63623ba6b33576
talumbau <tj.alumbaugh@continuum.io>
2016-05-10 07:54:31 -0500

    Fix multi-value params with multiple wildcards
    - indexing was incorrect in plugging in expanded defaults
    - add test for multiple wildcards in a multi-valued parameter

--- webapp/apps/taxbrain/tests/test_all.py
+++ webapp/apps/taxbrain/tests/test_all.py
@@ -150,6 +150,14 @@ class TaxInputTests(TestCase):
                [250000, None, None, None, None, None]]
         assert ans['_AMED_thd'] == exp
 
+    def test_package_up_multivalue_param_with_wildcard3(self):
+        values = {"AMED_thd_0": [u'*', u'*', 250000]}
+        ans = package_up_vars(values, first_budget_year=FBY)
+        exp = [[200000, 250000.0, 125000.0, 200000.0, 200000.0, 125000.0],
+               [200000, None, None, None, None, None],
+               [250000, None, None, None, None, None]]
+        assert ans['_AMED_thd'] == exp
+
 
     def test_package_up_vars_unicode_wildcards(self):
         exp = [0.0089999999999999993, 0.0089999999999999993, 0.018]
