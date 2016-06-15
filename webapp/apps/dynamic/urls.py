3788fee7017da1db8ff3a22299a0f221d1924a58
talumbau <tj.alumbaugh@continuum.io>
2016-06-14 21:21:04 -0500

    New partial equilibrium loading page
     - Make the partial equilibrium loading/results page the same style as
       for microsim

--- webapp/apps/dynamic/urls.py
+++ webapp/apps/dynamic/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from .views import (show_job_submitted, dynamic_input, dynamic_finished,
                     ogusa_results, dynamic_landing, dynamic_behavioral,
-                    behavior_results, behavior_output, edit_dynamic_behavioral,
+                    behavior_results, edit_dynamic_behavioral,
                     dynamic_elasticities, elastic_results, elastic_output,
                     edit_dynamic_elastic)
 
@@ -15,10 +15,8 @@ urlpatterns = patterns('',
     url(r'^macro/edit/(?P<pk>\d+)/', edit_dynamic_elastic, name='edit_dynamic_elastic'),
     url(r'^macro/(?P<pk>\d+)/', dynamic_elasticities, name='dynamic_elasticities'),
     url(r'^submitted/(?P<pk>\d+)/', show_job_submitted, name='show_job_submitted'),
-    # Redirect for a temporary page.
-    url(r'^processing/(?P<pk>\d+)/', behavior_results, name='behavior_results'),
     url(r'^macro_processing/(?P<pk>\d+)/', elastic_results, name='elastic_results'),
-    url(r'^behavior_results/(?P<pk>\d+)/', behavior_output, name='behavior_output'),
     url(r'^macro_results/(?P<pk>\d+)/', elastic_output, name='elastic_output'),
+    url(r'^behavior_results/(?P<pk>\d+)/', behavior_results, name='behavior_results'),
     url(r'^dynamic_finished/', dynamic_finished, name='dynamic_finished'),
 )
--- webapp/apps/dynamic/views.py
+++ webapp/apps/dynamic/views.py
@@ -1,4 +1,5 @@
 import json
+import pytz
 import datetime
 from urlparse import urlparse, parse_qs
 import os
@@ -17,7 +18,8 @@ from django.core import serializers
 from django.core.context_processors import csrf
 from django.core.exceptions import ValidationError
 from django.contrib.auth.decorators import login_required, permission_required
-from django.http import HttpResponseRedirect, HttpResponse, Http404, HttpResponseServerError
+from django.http import (HttpResponseRedirect, HttpResponse, Http404, HttpResponseServerError,
+                         JsonResponse)
 from django.shortcuts import render, render_to_response, get_object_or_404, redirect
 from django.template import loader, Context
 from django.template.context import RequestContext
@@ -35,7 +37,7 @@ from .models import (DynamicSaveInputs, DynamicOutputUrl,
 from ..taxbrain.models import TaxSaveInputs, OutputUrl
 from ..taxbrain.views import growth_fixup, benefit_surtax_fixup, make_bool
 from ..taxbrain.helpers import default_policy, taxcalc_results_to_tables, default_behavior
-from ..taxbrain.views import dropq_compute
+from ..taxbrain.views import dropq_compute, JOB_PROC_TIME_IN_SECONDS
 
 from .helpers import (default_parameters, job_submitted,
                       ogusa_results_to_tables, success_text,
@@ -232,7 +234,25 @@ def dynamic_behavioral(request, pk):
                 model.job_ids = denormalize(submitted_ids)
                 model.first_year = int(start_year)
                 model.save()
-                return redirect('behavior_results', model.pk)
+
+                unique_url = DynamicBehaviorOutputUrl()
+                if request.user.is_authenticated():
+                    current_user = User.objects.get(pk=request.user.id)
+                    unique_url.user = current_user
+                if unique_url.taxcalc_vers != None:
+                    pass
+                else:
+                    unique_url.taxcalc_vers = taxcalc_version
+
+                unique_url.unique_inputs = model
+                unique_url.model_pk = model.pk
+                cur_dt = datetime.datetime.utcnow()
+                future_offset = datetime.timedelta(seconds=((2 + max_q_length) * JOB_PROC_TIME_IN_SECONDS))
+                expected_completion = cur_dt + future_offset
+                unique_url.exp_comp_datetime = expected_completion
+                unique_url.save()
+
+                return redirect('behavior_results', unique_url.pk)
 
         else:
             # received POST but invalid results, return to form with errors
@@ -611,32 +631,6 @@ def elastic_results(request, pk):
 
 def behavior_results(request, pk):
     """
-    This view allows the app to wait for the taxcalc results to be
-    returned.
-    """
-    model = DynamicBehaviorSaveInputs.objects.get(pk=pk)
-    job_ids = model.job_ids
-    submitted_ids = normalize(job_ids)
-    if all(dropq_compute.dropq_results_ready(submitted_ids)):
-        model.tax_result = dropq_compute.dropq_get_results(submitted_ids)
-        model.creation_date = datetime.datetime.now()
-        model.save()
-
-        unique_url = DynamicBehaviorOutputUrl()
-        if request.user.is_authenticated():
-            current_user = User.objects.get(pk=request.user.id)
-            unique_url.user = current_user
-        unique_url.unique_inputs = model
-        unique_url.model_pk = model.pk
-        unique_url.save()
-
-        return redirect('behavior_output', unique_url.pk)
-
-    return render_to_response('dynamic/not_ready.html', {'raw_results':'raw_results'})
-
-
-def behavior_output(request, pk):
-    """
     This view handles the partial equilibrium results page.
     """
     try:
@@ -650,39 +644,84 @@ def behavior_output(request, pk):
         url.taxcalc_vers = taxcalc_version
         url.save()
 
-    output = url.unique_inputs.tax_result
-    first_year = url.unique_inputs.first_year
-    created_on = url.unique_inputs.creation_date
-    tables = taxcalc_results_to_tables(output, first_year)
-    tables["tooltips"] = {
-        'diagnostic': DIAGNOSTIC_TOOLTIP,
-        'difference': DIFFERENCE_TOOLTIP,
-        'payroll': PAYROLL_TOOLTIP,
-        'income': INCOME_TOOLTIP,
-        'base': BASE_TOOLTIP,
-        'reform': REFORM_TOOLTIP,
-        'expanded': EXPANDED_TOOLTIP,
-        'adjusted': ADJUSTED_TOOLTIP,
-        'bins': INCOME_BINS_TOOLTIP,
-        'deciles': INCOME_DECILES_TOOLTIP
-    }
-    dbsi = url.unique_inputs
-    is_registered = True if request.user.is_authenticated() else False
-    hostname = os.environ.get('BASE_IRI', 'http://www.ospc.org')
-    microsim_url = hostname + "/taxbrain/" + str(dbsi.micro_sim.pk)
+    model = url.unique_inputs
 
-    context = {
-        'locals':locals(),
-        'unique_url':url,
-        'taxcalc_version':taxcalc_version,
-        'tables': json.dumps(tables),
-        'created_on': created_on,
-        'first_year': first_year,
-        'is_registered': is_registered,
-        'is_behavior': True,
-        'microsim_url': microsim_url
-    }
+    if model.tax_result:
+        output = model.tax_result
+        first_year = model.first_year
+        created_on = model.creation_date
+        tables = taxcalc_results_to_tables(output, first_year)
+        tables["tooltips"] = {
+            'diagnostic': DIAGNOSTIC_TOOLTIP,
+            'difference': DIFFERENCE_TOOLTIP,
+            'payroll': PAYROLL_TOOLTIP,
+            'income': INCOME_TOOLTIP,
+            'base': BASE_TOOLTIP,
+            'reform': REFORM_TOOLTIP,
+            'expanded': EXPANDED_TOOLTIP,
+            'adjusted': ADJUSTED_TOOLTIP,
+            'bins': INCOME_BINS_TOOLTIP,
+            'deciles': INCOME_DECILES_TOOLTIP
+        }
+        is_registered = True if request.user.is_authenticated() else False
+        hostname = os.environ.get('BASE_IRI', 'http://www.ospc.org')
+        microsim_url = hostname + "/taxbrain/" + str(model.micro_sim.pk)
 
-    return render(request, 'taxbrain/results.html', context)
+        context = {
+            'locals':locals(),
+            'unique_url':url,
+            'taxcalc_version':taxcalc_version,
+            'tables': json.dumps(tables),
+            'created_on': created_on,
+            'first_year': first_year,
+            'is_registered': is_registered,
+            'is_behavior': True,
+            'microsim_url': microsim_url
+        }
+        return render(request, 'taxbrain/results.html', context)
 
+    else:
 
+        #url = DynamicBehaviorSaveInputs.objects.get(pk=pk)
+        job_ids = model.job_ids
+        jobs_to_check = model.jobs_not_ready
+        if not jobs_to_check:
+            jobs_to_check = normalize(job_ids)
+        else:
+            jobs_to_check = normalize(jobs_to_check)
+
+        try:
+            jobs_ready = dropq_compute.dropq_results_ready(jobs_to_check)
+        except JobFailError as jfe:
+            print jfe
+            return render_to_response('taxbrain/failed.html')
+
+        if all(jobs_ready):
+            model.tax_result = dropq_compute.dropq_get_results(normalize(job_ids))
+            model.creation_date = datetime.datetime.now()
+            model.save()
+            return redirect(url)
+            return redirect('behavior_results', url.pk)
+        else:
+            jobs_not_ready = [sub_id for (sub_id, job_ready) in
+                                zip(jobs_to_check, jobs_ready) if not job_ready]
+            jobs_not_ready = denormalize(jobs_not_ready)
+            model.jobs_not_ready = jobs_not_ready
+            model.save()
+            if request.method == 'POST':
+                # if not ready yet, insert number of minutes remaining
+                exp_comp_dt = url.exp_comp_datetime
+                utc_now = datetime.datetime.utcnow()
+                utc_now = utc_now.replace(tzinfo=pytz.utc)
+                dt = exp_comp_dt - utc_now
+                exp_num_minutes = dt.total_seconds() / 60.
+                exp_num_minutes = round(exp_num_minutes, 2)
+                exp_num_minutes = exp_num_minutes if exp_num_minutes > 0 else 0
+                if exp_num_minutes > 0:
+                    return JsonResponse({'eta': exp_num_minutes}, status=202)
+                else:
+                    return JsonResponse({'eta': exp_num_minutes}, status=200)
+
+            else:
+                print "rendering not ready yet"
+                return render_to_response('dynamic/not_ready.html', {'eta': '100'}, context_instance=RequestContext(request))
