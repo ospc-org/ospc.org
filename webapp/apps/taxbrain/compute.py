08202f078e64064bca5875e7b34b96c6b6897dfe
Peter Steinberg <psteinberg@continuum.io>
2016-08-15 14:36:26 -0700

    fix regression in compute.py RE num_budget_years

--- webapp/apps/taxbrain/compute.py
+++ webapp/apps/taxbrain/compute.py
@@ -83,7 +83,7 @@ class DropqCompute(object):
         print "user_mods is ", user_mods
         print "submit work"
         user_mods={first_budget_year:user_mods}
-        years = self._get_years(start_budget_year, first_budget_year)
+        years = self._get_years(start_budget_year, num_years)
         wnc, created = WorkerNodesCounter.objects.get_or_create(singleton_enforce=1)
         dropq_worker_offset = wnc.current_offset
         if dropq_worker_offset > len(DROPQ_WORKERS):
@@ -132,9 +132,9 @@ class DropqCompute(object):
 
         return job_ids, max_queue_length
 
-    def _get_years(self, start_budget_year, first_budget_year):
+    def _get_years(self, start_budget_year, num_years):
         if start_budget_year is not None:
-            return list(range(start_budget_year, self.num_budget_years))
+            return list(range(start_budget_year, num_years))
         # The following is just a dummy year for btax
         # Btax is not currently running in separate years, I don't think.
         return [first_budget_year]
