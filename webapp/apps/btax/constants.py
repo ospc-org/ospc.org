fd05435bcd4e5c42c322c986e915d469323b9c36
bcollins <bcollins@continuum.io>
2016-10-20 09:37:44 -0500

    added constants file for btax tooltips

---
+++ webapp/apps/btax/constants.py
@@ -0,0 +1,4 @@
+METR_TOOLTIP = ("Marginal effective tax rate on new investments. The METR includes the impact of the first level of taxation on the incentives to invest."
+METTR_TOOLTIP = "Marginal effective total tax rate on new investments. The METTR includes the impact of all levels of taxation (both on business entities and savers) on the incentives to invest."
+COC_TOOLTIP = "The cost of capital is calculated as the net-of-depreciation, before-tax rate of return."
+DPRC_TOOLTIP = "Net present value of depreciation deductions."
