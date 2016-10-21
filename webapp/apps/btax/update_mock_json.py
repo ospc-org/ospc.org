1fa9123865b546fa2bcbc04c08e0e2a1e37a178e
bcollins <bcollins@continuum.io>
2016-10-21 16:13:52 -0500

    updated mock json

---
+++ webapp/apps/btax/update_mock_json.py
@@ -0,0 +1,24 @@
+import json
+import random
+
+with open('__mock_btax_result.json') as f:
+    data = f.read()
+    data = json.loads(data)
+
+cats = ['Major Grouping 1',
+        'Major Grouping 2',
+        'Major Grouping 3',
+        'Major Grouping 4',
+        'Major Grouping 5',
+        'Major Grouping 6']
+
+for h in data.keys():
+    if h.startswith('asset') or h.startswith('industry'):
+        for i in data[h].keys():
+            for j in data[h][i]['rows']:
+                j['major_grouping'] = random.choice(cats)
+                j['summary_c'] = -999
+                j['summary_nc'] = -999
+    
+with open('mock_btax_result.json','w') as f:
+    f.write(json.dumps(data))
