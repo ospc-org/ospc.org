4e518339c414e2f37294681ee8164f9beca52939
Brittain Hard <brittainhard@gmail.com>
2017-01-13 15:23:21 -0600

    Separate out into function, first stab at subsections.

--- scratch.py
+++ scratch.py
@@ -7,15 +7,23 @@ with open("current_law_policy.json", "r") as f:
     a = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
 
-grouped_fields = []
-for x in a:
-    section_name = dict(a[x]).get("section_1")
-    if section_name:
-        section_name = section_name.lower()
-        section = next((item for item in grouped_fields if section_name in item.keys()), None)
-        if not section:
-            grouped_fields.append({section_name: [{x: dict(a[x])}]})
-        else:
-            section[section_name].append({x: dict(a[x])})
+def parse_sub_category(field_section):
+    output_list = []
+    for x in field_section:
+        print(x)
 
-pprint(grouped_fields[0])
+def parse_category(json_output):
+    output = []
+    for x in json_output:
+        section_name = dict(json_output[x]).get("section_1")
+        if section_name:
+            section_name = section_name.lower()
+            section = next((item for item in output if section_name in item.keys()), None)
+            if not section:
+                output.append({section_name: [{x: dict(json_output[x])}]})
+            else:
+                section[section_name].append({x: dict(json_output[x])})
+    return output
+
+b = parse_category(a)[0]
+c = parse_sub_category(b.values()[0])
