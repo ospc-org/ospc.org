7a709890c549aa029a131dc38b7ab1291868f990
Brittain Hard <brittainhard@gmail.com>
2017-01-26 10:32:56 -0600

    Added missing fields to model form.

---
+++ webapp/apps/taxbrain/migrations/0041_auto_20170126_1620.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import webapp.apps.taxbrain.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taxbrain', '0040_taxsaveinputs_id_benefitsurtax_em'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='taxsaveinputs',
+            name='ID_BenefitSurtax_em',
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='ACTC_Income_thd_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='AGI_surtax_thd_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='CDCC_c_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='CDCC_crt_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='CDCC_ps_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='CG_brk3_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='CTC_new_ps',
+            field=webapp.apps.taxbrain.models.CommaSeparatedField(default=None, max_length=200, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='EITC_InvestIncome_c',
+            field=webapp.apps.taxbrain.models.CommaSeparatedField(default=None, max_length=200, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='FST_AGI_thd_hi_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='FST_AGI_thd_lo_cpi',
+            field=models.NullBooleanField(default=None),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='ID_BenefitSurtax_em_0',
+            field=webapp.apps.taxbrain.models.CommaSeparatedField(default=None, max_length=200, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='ID_BenefitSurtax_em_1',
+            field=webapp.apps.taxbrain.models.CommaSeparatedField(default=None, max_length=200, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='ID_BenefitSurtax_em_2',
+            field=webapp.apps.taxbrain.models.CommaSeparatedField(default=None, max_length=200, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='taxsaveinputs',
+            name='ID_BenefitSurtax_em_3',
+            field=webapp.apps.taxbrain.models.CommaSeparatedField(default=None, max_length=200, null=True, blank=True),
+        ),
+    ]
