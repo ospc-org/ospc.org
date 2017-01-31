b1e4f13fbb204d1f75df48c7c365294f7fbf6ec7
Brittain Hard <brittainhard@gmail.com>
2017-01-31 09:03:16 -0600

    Fix links between sidebar and form.

--- webapp/apps/taxbrain/templatetags/strings.py
+++ webapp/apps/taxbrain/templatetags/strings.py
@@ -6,7 +6,10 @@ register = template.Library()
 def make_id(name):
     return "-".join(name.split())
 
+@register.filter
+def block_param_title(dict):
+    return dict.keys()[0].title()
 
 @register.filter
-def make_title(name):
-    return name.title()
+def block_param_id(dict):
+    return "-".join(dict.keys()[0].split())
