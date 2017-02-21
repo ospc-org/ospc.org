ba469d68dde40a3131881ac0a7175389d8f6756a
Brittain Hard <brittainhard@gmail.com>
2017-02-21 12:14:29 -0600

    Title for adjustments gallery page. Fixed image not showing up.

--- webapp/apps/pages/views.py
+++ webapp/apps/pages/views.py
@@ -82,9 +82,6 @@ def gallerypage(request):
         },
     })
 
-def border_adjustment_plot(request):
-    return render(request, 'pages/border_adjustment.html')
-
 def newspage(request):
     return redirect(BLOG_URL)
 
@@ -147,6 +144,13 @@ def widgetpage(request, widget_id):
         }
     })
 
+def border_adjustment_plot(request):
+    return render(request, 'pages/border_adjustment.html', {
+        'section': {
+            'title': 'Widget',
+        }
+    })
+
 @xframe_options_exempt
 def embedpage(request, widget_id, layout='landscape'):
     form = subscribeform(request)
@@ -198,7 +202,6 @@ def embedpage(request, widget_id, layout='landscape'):
         }
 
     return render(request, 'pages/embed.html', response_obj)
-
 
 def apps_landing_page(request):
     context = {'btax_version': BTAX_VERSION,
