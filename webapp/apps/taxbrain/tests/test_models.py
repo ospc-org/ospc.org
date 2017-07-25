65cf4e325b499c6e6476a1c8189a09d7cebd72bc
Brittain Hard <brittainhard@gmail.com>
2017-07-25 16:20:52 -0500

    Tests for character limit.

---
+++ webapp/apps/taxbrain/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.test import Client
+
+from ..models import TaxSaveInputs, JSONReformTaxCalculator
+
+START_YEAR = 2016
+
+
+class TaxBrainJSONReformModelTest(TestCase):
+    """Test taxbrain JSONReformTaxCalculator."""
+
+    def setUp(self):
+        # Every test needs a client.
+        self.test_string = "".join(["1" for x in range(100000)])
+
+    def test_create_potato(self):
+        self.reforms = JSONReformTaxCalculator.objects.create(
+            reform_text=self.test_string,
+            raw_reform_text=self.test_string,
+            assumption_text=self.test_string,
+            raw_assumption_text=self.test_string
+        )
