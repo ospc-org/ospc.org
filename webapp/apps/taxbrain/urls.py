4cabc3e322391eab4cc4fcee6fe61f5f7c5984cc
Henry Doupe <hdoupe@yahoo.com>
2017-09-18 18:13:46 -0400

    Remove submit_micro function

--- webapp/apps/taxbrain/urls.py
+++ webapp/apps/taxbrain/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from .views import (personal_results, output_detail, csv_input, csv_output,
-                    pdf_view, edit_personal_results, submit_micro, file_input)
+                    pdf_view, edit_personal_results, file_input)
 
 
 
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/output.csv/$', csv_output, name='csv_output'),
     url(r'^(?P<pk>\d+)/input.csv/$', csv_input, name='csv_input'),
     url(r'^(?P<pk>\d+)/', output_detail, name='output_detail'),
-    url(r'^submit/(?P<pk>\d+)/', submit_micro, name='submit_micro'),
     url(r'^pdf/$', pdf_view),
     url(r'^edit/(?P<pk>\d+)/', edit_personal_results, name='edit_personal_results'),
 )
