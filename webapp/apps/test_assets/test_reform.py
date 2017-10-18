fe4e81f108d377778c0085791a87cf0e62c41c00
Henry Doupe <hdoupe@yahoo.com>
2017-10-18 18:24:55 -0400

    Add first iteration of unit tests for param formatting functions

--- webapp/apps/test_assets/test_reform.py
+++ webapp/apps/test_assets/test_reform.py
@@ -157,3 +157,212 @@ warning_reform = """
     }
 }
 """
+
+test_coverage_fields = {
+    '_state': "<django.db.models.base.ModelState object at 0x10c764950>",
+    'creation_date': "datetime.datetime(2015, 1, 1, 0, 0)",
+    'id': 64,
+    'quick_calc': False,
+    'first_year': 2017,
+    'CG_nodiff': [False],
+    'FICA_ss_trt': [u'*', 0.1, u'*', 0.2],
+    'STD_0': [8000.0, '*', 10000.0],
+    'ID_BenefitSurtax_Switch_0': [True],
+    'ID_Charity_c_cpi': True,
+    'EITC_rt_2': [1.0]
+}
+
+test_coverage_reform = {
+    '_CG_nodiff': {'2017': [False]},
+    '_FICA_ss_trt': {'2020': [0.2], '2018': [0.1]},
+    '_STD_single': {'2017': [8000.0], '2019': [10000.0]},
+    '_ID_Charity_c_cpi': {'2017': True},
+    '_ID_BenefitSurtax_Switch_medical': {'2017': [True]},
+    '_EITC_rt_2kids': {'2017': [1.0]}
+}
+
+test_coverage_map_back_to_tb = {
+
+}
+
+
+test_coverage_json_reform = """
+{
+    "policy": {
+        "_ID_BenefitSurtax_Switch_charity": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_ALD_InvInc_ec_base_RyanBrady": {
+            "2017": [
+                false
+            ]
+        },
+        "_ID_BenefitSurtax_Switch_interest": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_EITC_indiv": {
+            "2017": [
+                false
+            ]
+        },
+        "_ID_BenefitSurtax_Switch_misc": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_ID_BenefitCap_Switch_charity": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_STD_single": {
+            "2017": [
+                10000.0
+            ]
+        },
+        "_II_no_em_nu18": {
+            "2017": [
+                false
+            ]
+        },
+        "_ID_BenefitSurtax_Switch_realestate": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_ID_BenefitCap_Switch_misc": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_CG_nodiff": {
+            "2017": [
+                false
+            ]
+        },
+        "_ID_BenefitSurtax_Switch_statelocal": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_ID_BenefitCap_Switch_medical": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_FICA_ss_trt": {
+            "2020": [
+                0.2
+            ],
+            "2018": [
+                0.1
+            ]
+        },
+        "_ID_BenefitCap_Switch_casualty": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_ID_Charity_c_cpi": {
+            "2017": true
+        },
+        "_ID_BenefitCap_Switch_statelocal": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_EITC_rt_2kids": {
+            "2017": [
+                1.0
+            ]
+        },
+        "_ID_BenefitSurtax_Switch_casualty": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_NIIT_PT_taxed": {
+            "2017": [
+                false
+            ]
+        },
+        "_ID_BenefitSurtax_Switch_medical": {
+            "2017": [
+                1.0
+            ]
+        },
+        "_ID_BenefitCap_Switch_interest": {
+            "2017": [
+                0.0
+            ]
+        },
+        "_CTC_new_refund_limited": {
+            "2017": [
+                false
+            ]
+        },
+        "_ID_BenefitCap_Switch_realestate": {
+            "2017": [
+                0.0
+            ]
+        }
+    }
+}
+"""
+
+test_coverage_json_assumptions = """
+{
+    "growdiff_response": {},
+    "consumption": {},
+    "behavior": {},
+    "growdiff_baseline": {}
+}
+"""
+
+test_coverage_map_back_to_tb = {
+    u'_ID_BenefitSurtax_Switch_charity': 'ID_BenefitSurtax_Switch_6',
+    '_ALD_InvInc_ec_base_RyanBrady': 'ALD_InvInc_ec_base_RyanBrady',
+    u'_ID_BenefitSurtax_Switch_interest': 'ID_BenefitSurtax_Switch_5',
+    '_EITC_indiv': 'EITC_indiv',
+    u'_ID_BenefitSurtax_Switch_misc': 'ID_BenefitSurtax_Switch_4',
+    u'_ID_BenefitCap_Switch_charity': 'ID_BenefitCap_Switch_6',
+    u'_STD_single': 'STD_0',
+    '_II_no_em_nu18': 'II_no_em_nu18',
+    u'_ID_BenefitSurtax_Switch_realestate': 'ID_BenefitSurtax_Switch_2',
+    u'_ID_BenefitCap_Switch_misc': 'ID_BenefitCap_Switch_4',
+    '_CG_nodiff': 'CG_nodiff',
+    u'_ID_BenefitSurtax_Switch_statelocal': 'ID_BenefitSurtax_Switch_1',
+    u'_ID_BenefitCap_Switch_medical': 'ID_BenefitCap_Switch_0',
+    '_FICA_ss_trt': 'FICA_ss_trt',
+    u'_ID_BenefitCap_Switch_casualty': 'ID_BenefitCap_Switch_3',
+    '_ID_Charity_c_cpi': 'ID_Charity_c_cpi',
+    u'_ID_BenefitCap_Switch_statelocal': 'ID_BenefitCap_Switch_1',
+    u'_EITC_rt_2kids': 'EITC_rt_2',
+    u'_ID_BenefitSurtax_Switch_casualty': 'ID_BenefitSurtax_Switch_3',
+    '_NIIT_PT_taxed': 'NIIT_PT_taxed',
+    u'_ID_BenefitSurtax_Switch_medical': 'ID_BenefitSurtax_Switch_0',
+    u'_ID_BenefitCap_Switch_interest': 'ID_BenefitCap_Switch_5',
+    '_CTC_new_refund_limited': 'CTC_new_refund_limited',
+    u'_ID_BenefitCap_Switch_realestate': 'ID_BenefitCap_Switch_2'
+}
+
+
+test_coverage_exp_read_json_reform = {
+    2017: {u'_EITC_rt': [[0.0765, 0.34, 1.0, 0.45]],
+           u'_NIIT_PT_taxed': [False],
+           u'_ID_BenefitCap_Switch': [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]],
+           u'_ALD_InvInc_ec_base_RyanBrady': [False],
+           u'_EITC_indiv': [False],
+           u'_ID_BenefitSurtax_Switch': [[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]],
+           u'_STD': [[10000.0, 12700.0, 6350.0, 9350.0, 12700.0]],
+           u'_II_no_em_nu18': [False],
+           u'_ID_Charity_c_cpi': True,
+           u'_CG_nodiff': [False],
+           u'_CTC_new_refund_limited': [False]},
+    2018: {u'_FICA_ss_trt': [0.1]},
+    2020: {u'_FICA_ss_trt': [0.2]}
+}
