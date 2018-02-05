47003dfefaa6a3fda82a66931bf31ff0799a8014
Henry Doupe <hdoupe@yahoo.com>
2018-02-05 14:03:27 -0500

    temp fix to print stack trace on warnings

---
+++ webapp/apps/__init__.py
@@ -0,0 +1,11 @@
+import traceback
+import warnings
+import sys
+
+def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+
+    log = file if hasattr(file,'write') else sys.stderr
+    traceback.print_stack(file=log)
+    log.write(warnings.formatwarning(message, category, filename, lineno, line))
+
+warnings.showwarning = warn_with_traceback
