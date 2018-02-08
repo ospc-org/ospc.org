16b75240d41e1540275e0d738df0e749cceb7a64
Henry Doupe <hdoupe@yahoo.com>
2018-02-08 13:45:47 -0500

    Automate local dev database creation

--- webapp_env.sh
+++ webapp_env.sh
@@ -1,3 +1,22 @@
+# if you want to use a user besides postgres,
+# run `DATABASE_NAME=yourusername source webapp_env.sh`
+export DATABASE_NAME=policybrain_local_database
+if [ -z ${DATABASE_USER+x} ]
+then
+    echo using database username postgres
+    export DATABASE_USER=postgres
+else
+    echo using database username $DATABASE_USER
+fi
+
+export DATABASE_URL=postgresql://localhost/$DATABASE_NAME
+if ! psql -l | grep $DATABASE_NAME
+then
+    echo creating database $DATABASE_NAME under username $DATABASE_USER
+    createdb $DATABASE_NAME -U $DATABASE_USER
+fi
+
+
 export NUM_BUDGET_YEARS=10
 export DEV_DEBUG=True
 export HTML_MINIFY=True
