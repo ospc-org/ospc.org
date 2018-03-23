53cad4d66f20d5ba25f0db905bf87201ddcf7074
Henry Doupe <hdoupe@yahoo.com>
2018-03-23 15:48:06 -0400

    Try adding channels at the start

--- python_env_build.sh
+++ python_env_build.sh
@@ -1,22 +1,14 @@
-echo 'installing with conda...'
-conda create -n pb_env python=2.7.14 \
-    taxcalc=0.17.0 \
-    btax=0.2.2 \
-    ogusa=0.5.8 \
-    numba>=0.33.0 \
-    pandas>=0.22.0 \
-    bokeh=0.12.7 \
-    nomkl \
-    gevent \
-    pillow \
-    pyparsing \
-    -c ospc \
-    -c ospc/label/dev \
-    -c conda-forge \
-
+echo 'appending channels'
+conda config --append channels ospc/label/dev
+conda config --append channels ospc
+conda config --append channels conda-forge
+echo 'creating environment pb_env and installing conda packages'
+conda create -n pb_env python=2.7.14 taxcalc=0.17.0 btax=0.2.2 ogusa=0.5.8 numba>=0.33.0 pandas>=0.22.0 bokeh=0.12.7 nomkl gevent pillow pyparsing
 echo 'successfull created env: pb_env'
-echo 'activating pb_env and pip installing remaining requirements'
+
+echo 'activating env: pb_env'
 source activate pb_env
 
+echo 'pip installing remaining requirements'
 pip install -r requirements.txt
 pip install -r requirements_dev.txt
