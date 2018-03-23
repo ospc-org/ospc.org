fa6295790c8753bab9389d276223d902b602cd15
Henry Doupe <hdoupe@yahoo.com>
2018-03-23 15:56:06 -0400

    Try doing a conda update at the beginning

--- python_env_build.sh
+++ python_env_build.sh
@@ -1,7 +1,11 @@
+echo 'updating conda'
+conda update -n base -c defaults
+
 echo 'appending channels'
 conda config --append channels ospc/label/dev
 conda config --append channels ospc
 conda config --append channels conda-forge
+
 echo 'creating environment pb_env and installing conda packages'
 conda create -n pb_env python=2.7.14 taxcalc=0.17.0 btax=0.2.2 ogusa=0.5.8 numba>=0.33.0 pandas>=0.22.0 bokeh=0.12.7 nomkl gevent pillow pyparsing
 echo 'successfull created env: pb_env'
