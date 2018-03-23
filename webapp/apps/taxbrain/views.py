e74064c513e053d51bc86e890ca2b6d9161b03fa
Henry Doupe <hdoupe@yahoo.com>
2018-03-23 17:29:05 -0400

    Update job proc time from 30s to 90s

--- webapp/apps/taxbrain/views.py
+++ webapp/apps/taxbrain/views.py
@@ -65,7 +65,7 @@ tcversion_info = taxcalc._version.get_versions()
 
 TAXCALC_VERSION = tcversion_info['version']
 
-JOB_PROC_TIME_IN_SECONDS = 30
+JOB_PROC_TIME_IN_SECONDS = 90
 
 OUT_OF_RANGE_ERROR_MSG = ("Some fields have warnings or errors. Values "
                           "outside of suggested ranges will be accepted if "
