19ca1a1d472625839059453df207adc808cc70ea
Henry Doupe <hdoupe@yahoo.com>
2018-03-23 16:16:20 -0400

    Perhaps conda likes yml files more than one-liners

--- python_env_build.sh
+++ python_env_build.sh
@@ -1,10 +1,5 @@
-echo 'appending channels'
-conda config --append channels ospc/label/dev
-conda config --append channels ospc
-conda config --append channels conda-forge
-
 echo 'creating environment pb_env and installing conda packages'
-conda create -n pb_env python=2.7.14 taxcalc=0.17.0 btax=0.2.2 ogusa=0.5.8 numba>=0.33.0 pandas>=0.22.0 bokeh=0.12.7 nomkl gevent pillow pyparsing
+conda env create
 echo 'successfull created env: pb_env'
 
 echo 'activating env: pb_env'
