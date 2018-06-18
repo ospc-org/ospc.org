849b054955f64959389adb6461de8e4201e77a4c
Henry Doupe <hdoupe@yahoo.com>
2018-06-18 11:49:53 -0400

    Use one script to build, push, and release all images

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pushd distributed
+
 echo 'building images...'
 docker build -t opensourcepolicycenter/distributed:$TAG ./ --build-arg PUF_TOKEN=$(cat ~/.ospc_anaconda_token)
 docker build --build-arg TAG=$TAG -t opensourcepolicycenter/flask:$TAG --file Dockerfile.flask ./
@@ -9,3 +11,13 @@ echo 'pushing images...'
 docker push opensourcepolicycenter/distributed:$TAG
 docker push opensourcepolicycenter/flask:$TAG
 docker push opensourcepolicycenter/celery:$TAG
+
+popd
+
+echo 'building web image...'
+docker build --build-arg NEW_RELIC_TOKEN=$(cat ~/newrelic-$VERSION) -t opensourcepolicycenter/web:$TAG ./
+
+echo 'tagging and pushing web image...'
+docker tag opensourcepolicycenter/web:$TAG registry.heroku.com/ospc-$VERSION/web
+docker push registry.heroku.com/ospc-$VERSION/web
+heroku container:release web -a ospc-$VERSION
