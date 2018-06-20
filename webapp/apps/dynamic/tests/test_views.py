9a923a99158b17b813f26dcb4c1bea9885a4611e
Lucas Szwarcberg <lucas.szwarcberg@gmail.com>
2018-06-20 10:10:51 -0400

    Change assertTrue and assertEqual to straight assert statements

--- webapp/apps/dynamic/tests/test_views.py
+++ webapp/apps/dynamic/tests/test_views.py
@@ -21,18 +21,12 @@ class DynamicViewsTests(object):
         # Every test needs a client.
         self.client = Client()
 
-    def assertEqual(self, a, b):
-        assert (a == b)
-
-    def assertTrue(self, a):
-        assert a
-
     def test_taxbrain_get(self):
         # Issue a GET request.
         response = self.client.get('/taxbrain/')
 
         # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
     def test_behavioral_post(self):
         #Monkey patch to mock out running of compute jobs
@@ -57,33 +51,33 @@ class DynamicViewsTests(object):
 
         response = self.client.post('/taxbrain/', data)
         # Check that redirect happens
-        self.assertEqual(response.status_code, 302)
+        assert (response.status_code == 302)
         # Go to results page
         link_idx = response.url[:-1].rfind('/')
-        self.assertTrue(response.url[:link_idx+1].endswith("taxbrain/"))
+        assert response.url[:link_idx+1].endswith("taxbrain/")
 
         # Link to dynamic simulation
         model_num = response.url[link_idx+1:-1]
         dynamic_landing = '/dynamic/{0}/?start_year={1}'.format(model_num, START_YEAR)
         response = self.client.get(dynamic_landing)
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
         # Go to behavioral input page
         dynamic_behavior = '/dynamic/behavioral/{0}/?start_year={1}'.format(model_num, START_YEAR)
         response = self.client.get(dynamic_behavior)
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
         # Do the partial equilibrium job submission
         pe_data = {'BE_inc': ['-0.4']}
         response = self.client.post(dynamic_behavior, pe_data)
-        self.assertEqual(response.status_code, 302)
+        assert (response.status_code == 302)
         print(response)
 
         #Check should get success this time
         response_success = self.client.get(response.url)
-        self.assertEqual(response_success.status_code, 200)
+        assert (response_success.status_code == 200)
         link_idx = response.url[:-1].rfind('/')
-        self.assertTrue(response.url[:link_idx+1].endswith("behavior_results/"))
+        assert response.url[:link_idx+1].endswith("behavior_results/")
 
     # Test whether default elasticity is used if no param is given
     @pytest.mark.parametrize(
@@ -113,31 +107,31 @@ class DynamicViewsTests(object):
 
         response = self.client.post('/taxbrain/', data)
         # Check that redirect happens
-        self.assertEqual(response.status_code, 302)
+        assert (response.status_code == 302)
         # Go to results page
         link_idx = response.url[:-1].rfind('/')
-        self.assertTrue(response.url[:link_idx+1].endswith("taxbrain/"))
+        assert response.url[:link_idx+1].endswith("taxbrain/")
 
         # Link to dynamic simulation
         model_num = response.url[link_idx+1:-1]
         dynamic_landing = '/dynamic/{0}/?start_year={1}'.format(model_num, START_YEAR)
         response = self.client.get(dynamic_landing)
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
         # Go to macro input page
         dynamic_egdp = '/dynamic/macro/{0}/?start_year={1}'.format(model_num, START_YEAR)
         response = self.client.get(dynamic_egdp)
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
         # Do the elasticity job submission
         response = self.client.post(dynamic_egdp, el_data)
-        self.assertEqual(response.status_code, 302)
+        assert (response.status_code == 302)
         print(response)
 
         #Check that we get success this time
         response_success = self.client.get(response.url)
-        self.assertEqual(response_success.status_code, 200)
-        self.assertTrue(response.url[:-2].endswith("macro_results/"))
+        assert (response_success.status_code == 200)
+        assert response.url[:-2].endswith("macro_results/")
 
     def test_elastic_failed_job(self):
         #Monkey patch to mock out running of compute jobs
@@ -163,33 +157,32 @@ class DynamicViewsTests(object):
 
         response = self.client.post('/taxbrain/', data)
         # Check that redirect happens
-        self.assertEqual(response.status_code, 302)
+        assert (response.status_code == 302)
         # Go to results page
         link_idx = response.url[:-1].rfind('/')
-        self.assertTrue(response.url[:link_idx+1].endswith("taxbrain/"))
+        assert response.url[:link_idx+1].endswith("taxbrain/")
 
         # Link to dynamic simulation
         model_num = response.url[link_idx+1:-1]
         dynamic_landing = '/dynamic/{0}/?start_year={1}'.format(model_num, START_YEAR)
         response = self.client.get(dynamic_landing)
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
         # Go to macro input page
         dynamic_egdp = '/dynamic/macro/{0}/?start_year={1}'.format(model_num, START_YEAR)
         response = self.client.get(dynamic_egdp)
-        self.assertEqual(response.status_code, 200)
+        assert (response.status_code == 200)
 
         # Do the elasticity job submission
         el_data = {'elastic_gdp': ['0.55']}
         response = self.client.post(dynamic_egdp, el_data)
-        self.assertEqual(response.status_code, 302)
+        assert (response.status_code == 302)
         print(response)
 
         #Check that we get success this time
         response_success = self.client.get(response.url)
-        self.assertEqual(response_success.status_code, 200)
-        self.assertTrue(response.url[:-2].endswith("macro_results/"))
+        assert (response_success.status_code == 200)
+        assert response.url[:-2].endswith("macro_results/")
         response = self.client.get(response.url)
         # Make sure the failure message is in the response
-        self.assertTrue("Your calculation failed"
-                        in response.content.decode('utf-8'))
+        assert ("Your calculation failed" in response.content.decode('utf-8'))
