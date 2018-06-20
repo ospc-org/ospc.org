c7d75aef3de7ab8b1bd0f6a85d3f48206821ee9f
Henry Doupe <hdoupe@yahoo.com>
2018-06-20 17:21:51 -0400

    Add helper function to rename stringified int keys

--- webapp/apps/taxbrain/helpers.py
+++ webapp/apps/taxbrain/helpers.py
@@ -674,6 +674,22 @@ def rename_keys(rename_dict, map_dict):
             rename_dict[new_label] = rename_keys(rename_dict.pop(k), map_dict)
     return rename_dict
 
+def json_int_key_encode(rename_dict):
+    """
+    Recursively rename integer value keys if they are casted to strings
+    via JSON encoding
+
+    returns: dict with new keys
+    """
+    if isinstance(rename_dict, dict):
+        for k in list(rename_dict.keys()):
+            if hasattr(k, 'isdigit') and k.isdigit():
+                new_label = int(k)
+            else:
+                new_label = k
+            rename_dict[new_label] = json_int_key_encode(rename_dict.pop(k))
+    return rename_dict
+
 
 def reorder_lists(results, reorder_ix_map, table_names):
     """
