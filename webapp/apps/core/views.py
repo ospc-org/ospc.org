3bfb235be593ac7e32a1404e547af1cdb2dc162c
Henry Doupe <hdoupe@yahoo.com>
2018-07-18 15:09:55 -0400

    Fill in outputs view a little more

--- webapp/apps/core/views.py
+++ webapp/apps/core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .models import CoreRun
+from .compute import Compute
 
 
-def get_result_context(model, request, url):
+def get_result_context(request, model, url):
     """
     generate context from request object and model and url model objects
 
@@ -30,4 +34,23 @@ def outputs(request, id):
     returns: data to be rendered and displayed
     """
 
-    return render('appropriate html page')
+    model = get_object_or_404(CoreRun, id=id)
+
+    compute = Compute()
+
+    if model.result is not None:
+        context = get_result_context(request, model)
+        return render('outputs.html', context)
+    elif model.error_message is not None:
+        context = {'error': model.error_message}
+        return render('failed.html', context)
+    else:
+        jobs_to_complete = compute.ready(model.job_ids)
+        if jobs_to_complete == 0:
+            result = compute.get_result(model.job_ids)
+            model.result = result
+            model.save()
+            return JsonResponse({'eta': 0}, status=202)
+        else:
+            eta = compute.eta(model.job_ids)
+            return JsonResponse({'eta': eta}, status=202)
