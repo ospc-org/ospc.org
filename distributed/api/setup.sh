629baee4f5ec3a7d46c41c14e6024fedd4ff1264
Henry Doupe <hdoupe@yahoo.com>
2018-08-16 10:58:39 -0400

    uncomment first line and rename setup script

--- distributed/api/setup.sh
+++
@@ -1,5 +0,0 @@
-
-#pushd ../Tax-Calculator && conda env create && source activate taxcalc-dev && pip install -e . && popd
-pushd ../B-Tax && pip install -e . && popd
-pip install -r requirements.txt
-pip install -e .
