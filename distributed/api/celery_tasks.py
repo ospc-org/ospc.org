9cb659585dd152bc730cf62eacac84ecc5c50388
Henry Doupe <hdoupe@yahoo.com>
2018-08-16 10:50:58 -0400

    Add tests, elast attempt, and local setup script

--- distributed/api/celery_tasks.py
+++ distributed/api/celery_tasks.py
@@ -53,7 +53,7 @@ def taxbrain_postprocess(ans):
     for year_data in ans:
         for key, value in year_data.items():
             all_to_process[key] += value
-    results = taxcalc.tbi.postprocess(all_to_process)
+    results = taxcalc.tbi.run_taxcalc_years_aggregation(all_to_process)
     # Add taxcalc version to results
     vinfo = taxcalc._version.get_versions()
     results['taxcalc_version'] = vinfo['version']
@@ -76,34 +76,42 @@ def dropq_task_small_async(year, user_mods, first_budget_year,
 
 
 @celery_app.task(name='api.celery_tasks.elasticity_gdp_task_async')
-def elasticity_gdp_task_async(year_n, user_mods, start_year,
-                              gdp_elasticity, use_puf_not_cps=True):
-    print("kw to dropq", dict(
-        year_n=year_n,
-        start_year=int(start_year),
-        use_puf_not_cps=use_puf_not_cps,
-        use_full_sample=True,
-        user_mods=user_mods,
-        gdp_elasticity=gdp_elasticity
-    ))
+def elasticity_gdp_task_async(year_n, start_year,
+                                 use_puf_not_cps,
+                                 use_full_sample,
+                                 user_mods,
+                                 gdp_elasticity,
+                                 return_dict=True):
 
     gdp_elast_i = taxcalc.tbi.run_nth_year_gdp_elast_model(
         year_n=year_n,
-        start_year=int(first_budget_year),
+        start_year=start_year,
         use_puf_not_cps=use_puf_not_cps,
-        use_full_sample=True,
+        use_full_sample=use_full_sample,
         user_mods=user_mods,
-        gdp_elasticity=gdp_elasticity
+        gdp_elasticity=gdp_elasticity,
+        return_dict=True
     )
+    print(gdp_elast_i)
 
-    results = {'elasticity_gdp': gdp_elast_i}
+    return gdp_elast_i
+
+
+@celery_app.task(name='api.celery_tasks.taxbrain_elast_postprocess')
+def taxbrain_elast_postprocess(ans):
+    print(ans)
+    all_to_process = defaultdict(list)
+    for year_data in ans:
+        for key, value in year_data.items():
+            all_to_process[key] += value
+    results = taxcalc.tbi.run_taxcalc_years_aggregation(all_to_process, ids=('gdp_effect',),
+                                      labels={'gdp_effect': 'GDP Title'})
     # Add taxcalc version to results
     vinfo = taxcalc._version.get_versions()
     results['taxcalc_version'] = vinfo['version']
+    # TODO: Make this the distributed app version, not the TC version
     results['dropq_version'] = vinfo['version']
-
-    json_res = json.dumps(results)
-    return json_res
+    return json.dumps(results)
 
 
 @celery_app.task(name='api.celery_tasks.btax_async')
---
+++ distributed/api/tests/test_celery.py
@@ -0,0 +1,79 @@
+import pytest
+from celery import chord
+
+from api.celery_tasks import (elasticity_gdp_task_async,
+                              taxbrain_elast_postprocess,
+                              dropq_task_small_async,
+                              taxbrain_postprocess)
+
+@pytest.fixture(scope='session')
+def celery_config():
+    return {
+        'broker_url': 'redis://localhost:6379',
+        'result_backend': 'redis://localhost:6379',
+        'task_serializer': 'json',
+        'accept_content': ['msgpack', 'json']}
+
+def test_elast_endpoint(celery_worker):
+    elast_params = {
+        'year_n': 1,
+        'user_mods': {
+            'policy': {2017: {'_FICA_ss_trt': [0.1]}},
+            'behavior': {},
+            'growdiff_response': {},
+            'consumption': {},
+            'growdiff_baseline': {},
+            'growmodel': {}},
+        'gdp_elasticity': 0.3,
+        'use_puf_not_cps': True,
+        'start_year': 2017,
+        'use_full_sample':True,
+        'return_dict': True
+    }
+
+    inputs = []
+    for i in range(1, 2):
+        inputs.append(dict(elast_params, **{'year_n': i}))
+    print(inputs)
+    # print('celery_worker', celery_worker)
+    # task = elasticity_gdp_task_async.apply_async(kwargs=data)
+    # r = task.get()
+    # print(r)
+    compute_task = elasticity_gdp_task_async
+    postprocess_task = taxbrain_elast_postprocess
+    result = (chord(compute_task.signature(kwargs=i, serializer='msgpack')
+              for i in inputs))(postprocess_task.signature(
+                serializer='msgpack'))
+    print(result.get())
+
+
+def test_taxcalc_endpoint(celery_worker):
+    tc_params = {
+        'user_mods': {
+            "policy": {
+                2017: {"_FICA_ss_trt": [0.1]}},
+            "consumption": {},
+            "behavior": {},
+            "growdiff_baseline": {},
+            "growdiff_response": {},
+            "growmodel": {}
+        },
+        'first_budget_year': 2017,
+        'use_puf_not_cps': False,
+        'year': 0
+    }
+
+    inputs = []
+    for i in range(1, 2):
+        inputs.append(dict(tc_params, **{'year': i}))
+    print(inputs)
+    # print('celery_worker', celery_worker)
+    # task = elasticity_gdp_task_async.apply_async(kwargs=data)
+    # r = task.get()
+    # print(r)
+    compute_task = dropq_task_small_async
+    postprocess_task = taxbrain_postprocess
+    result = (chord(compute_task.signature(kwargs=i, serializer='msgpack')
+              for i in inputs))(postprocess_task.signature(
+                serializer='msgpack'))
+    print(result.get())
