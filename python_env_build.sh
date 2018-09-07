1ed575e8bdb0605c67a0b75e9fd381263fba48ed
Henry Doupe <hdoupe@yahoo.com>
2018-09-07 14:24:40 -0400

    Resolve compatibility issues

--- python_env_build.sh
+++ python_env_build.sh
@@ -1,10 +1,14 @@
 echo 'creating environment pb_env and installing conda packages'
-conda env create
-echo 'successfull created env: pb_env'
+conda create -n pb_env python=3.6 --yes
+echo 'successfully created env: pb_env'
 
 echo 'activating env: pb_env'
 source activate pb_env
 
+echo 'installing conda packages'
+conda install -c ospc -c anaconda --file conda-requirements.txt --yes
+
+
 echo 'pip installing remaining requirements'
-pip install -r requirements.txt
-pip install -r requirements_dev.txt
+pip install -qr requirements.txt
+pip install -qr requirements_dev.txt
